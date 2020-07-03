890b8c9762d61d28f28b9667f554317b1171e7a4
samandeveloper <sn.mohaghegh@gmail.com>
2020-07-02 22:52:11 -0600

    move3

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = merge(common, {
       new TerserPlugin(),
       new HtmlWebpackPlugin({
         template: './index.html',
-        favicon: './src/assets/favicon.png',
+        favicon: './assets/favicon.png',
         minify: {
           removeAttributeQuotes: true,
           collapseWhitespace: true,
