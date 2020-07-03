176b5be11add878134dd6fead5fead8af238b2fa
samandeveloper <sn.mohaghegh@gmail.com>
2020-07-02 22:47:11 -0600

    move2

---
+++ index.js
@@ -0,0 +1,22 @@
+import initTilt from './src/js/tilt';
+import initSr from './src/js/sr';
+
+import './src/style/main.scss';
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
+});
+
+initSr();
+initTilt();
