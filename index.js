072471b064cd78031d9a9f994048f3268bbf1e97
samandeveloper <sn.mohaghegh@gmail.com>
2020-07-03 00:46:15 -0600

    move13

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import initTilt from './tilt';
 import initSr from './sr';
 
-import './main.scss';
+import 'main.scss';
 
 $('a[href^="#"]').on('click', function(event) {
   var target = $(this.getAttribute('href'));
