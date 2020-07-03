b57afe551f1da63b7bad925cc6f428ba7e752542
samandeveloper <sn.mohaghegh@gmail.com>
2020-07-03 00:06:05 -0600

    move10

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './index.html',
       favicon: './src/assets/favicon.png'
     })
   ],
